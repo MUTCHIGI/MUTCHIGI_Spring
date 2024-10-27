4c5fb0f5bd68a1bfe3b8851070f436c056a1b685
Youn <dzcoffee@users.noreply.github.com>
2024-10-27 22:51:31 +0900

    [server] feature : STOMP 방마다 연결 및 채팅 관리
    간단히 구현하였고 logic만 추가하면됨

--- MUTCHIGI/src/main/java/com/CAUCSD/MUTCHIGI/room/MusicChatController.java
+++ MUTCHIGI/src/main/java/com/CAUCSD/MUTCHIGI/room/MusicChatController.java
@@ -1,8 +1,11 @@
 package com.CAUCSD.MUTCHIGI.room;
 
+import com.CAUCSD.MUTCHIGI.room.chat.ChatEntity;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
@@ -12,32 +15,25 @@ import java.util.UUID;
 @Controller
 public class MusicChatController {
 
-    private final SimpMessagingTemplate messagingTemplate;
+    private ChatEntity chatEntity;
 
-    public MusicChatController(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
 
-    @EventListener
-    public void handleWebSocketConnectListener(SessionConnectEvent event) {
-        // UUID를 사용하여 roomId 생성
-        String roomId = UUID.randomUUID().toString();
-
-        // 클라이언트에게 roomId 전송 (적절한 경로로)
-        messagingTemplate.convertAndSend("/topic/roomId", roomId);
-
-        System.out.println("New room created with ID: " + roomId);
-    }
-
-    @MessageMapping("/send/{roomId}")
-    public void setMessage(String message, @DestinationVariable String roomId) {
-        System.out.println(message);
-        if (message.contains("특정키워드")) {
+    @MessageMapping("/send/{chatRoomId}")
+    @SendTo("/topic/{chatRoomId}")
+    public ChatEntity setMessage(@DestinationVariable long chatRoomId, @Payload ChatEntity chatEntity) {
+        System.out.println(chatEntity.getChatMessage());
+        /*
+        if (chatEntity.getChatMessage().contains("특정키워드")) {
             // 특정 작업 수행
             messagingTemplate.convertAndSend("/topic/messages/" + roomId, "특별한 응답 메시지");
         } else {
             messagingTemplate.convertAndSend("/topic/messages/" + roomId, message);
         }
 
+         */
+        ChatEntity chatEntity1 = new ChatEntity();
+        chatEntity1.setUserName("System");
+        chatEntity1.setChatMessage("확인용입니다.");
+        return chatEntity1;
     }
 }
--- MUTCHIGI/src/main/java/com/CAUCSD/MUTCHIGI/room/chat/ChatEntity.java
+++ MUTCHIGI/src/main/java/com/CAUCSD/MUTCHIGI/room/chat/ChatEntity.java
@@ -2,12 +2,11 @@ package com.CAUCSD.MUTCHIGI.room.chat;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
-@Component
 @Getter
 @Setter
 public class ChatEntity {
 
+    private String userName;
     private String chatMessage;
 }
